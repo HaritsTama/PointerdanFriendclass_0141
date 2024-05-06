55d2db060fdbc4a00209651b466dceae40334702
HaritsTama <nayotama1818@gmail.com>
2024-05-06 10:32:51 +0700

    deklarasi pointer reference

--- Pointer/Pointer.cpp
+++ Pointer/Pointer.cpp
@@ -15,4 +15,8 @@ int main()
 {
 	mahasiswa mhs{ 1 }; //Object mhs
 	mhs.showNim(); //Member access operator
+
+	mahasiswa& refMhs = mhs; //Pointer reference refMhs
+	refMhs.nim = 2; //Member access operator
+	mhs.showNim();
 }
